8a08203c80285d7aa552501a518d5645d875e270
rwoodwor <rwoodwor@scs.ryerson.ca>
2019-03-19 21:08:46 -0400

    Starting kruskals; added subset but may change

--- main.cpp
+++ main.cpp
@@ -8,27 +8,23 @@
 using namespace std;
 
 //Alias for edge and respective weight
-typedef pair<pair<int, int>, int> vect;
+typedef pair<pair<int, int>, int> Edge;
 
-
-//Function Declarations
-void print(vect edg);
-
-/*Creating Graph Struct
- *
+//Creating Graph Struct
+/*
  * *IF BEING USED: clean() FUNCTION MUST BE USED WHEN DONE*
  *
  */
 struct Graph {
     int verts, numEdg;
-    vect *edges;
+    Edge *edges;
     int n = 0;
 
     //Default constructor set to default test values
     Graph(){
         this->verts = 4;
         this->numEdg = 5;
-        this->edges = new vect[5];
+        this->edges = new Edge[this->numEdg];
 
         addEdge(0, 1, 5);
         addEdge(0, 2, 3);
@@ -41,7 +37,7 @@ struct Graph {
     Graph(int verts, int edges) {
         this->verts = verts;
         this->numEdg = edges;
-        this->edges = new vect[edges];
+        this->edges = new Edge[edges];
     }
 
     //Adds edge to graph, if attempting to add more edges than declared, edges wont be set
@@ -57,8 +53,10 @@ struct Graph {
         n++;
     }
 
+    //Helpers
+
     //Utilizing selection sort to sort graph edges by weight
-    void sort(int size, vect* edge){
+    void sort(int size, Edge* edge){
         int min;
 
         for(int i = 0; i < size -1; i++){
@@ -68,7 +66,7 @@ struct Graph {
                     min = j;
                 }
             }
-            vect t = edge[i];
+            Edge t = edge[i];
             edge[i]= edge[min];
             edge[min] = t;
         }
@@ -80,23 +78,61 @@ struct Graph {
         edges = NULL;
     }
 };
+struct Subset{
+    Edge edge;
+    Subset *next;
+};
 
+//Function Declarations
+void mst(Graph g);
+void printGraph(Edge edg);
 
 int main() {
     //Making default test graph
     Graph g = Graph();
 
-    //Sorting graph
-    g.sort(g.numEdg, g.edges);
-
-    //Printing graph to show sorted
-    for_each(g.edges, g.edges + g.numEdg, print);
+    //Running Kruskal's Algorithm
+    mst(g);
 
     //Cleaning memory used by graphs
     g.clean();
 }
 
+// Kruskal's Algorithm
+void mst(Graph g){
+    int source, destination;
+    int treeV = 0;
+
+    //Sorting by weight first
+    g.sort(g.numEdg, g.edges);
+
+    //Make list of trees (initially individual nodes) && Populate with vertices
+    Subset* sets = new Subset[g.numEdg];
+    for(int i = 0; i < g.numEdg; i++){
+        sets[i].edge = g.edges[i];
+        //cout<<"Set "<<i+1<<": {"<<sets[i].edge.first.first<<" "<<sets[i].edge.first.second<<"}"<<endl;
+    }
+
+    int num = 0;
+    int subCount = 0;
+    while(treeV < g.verts - 1){
+         Subset currEdge;
+         currEdge.edge = g.edges[num];
+        num++;
+
+        //InProgress
+        /*if(currEdge.first.first != currEdge.first.second){
+            sets[subCount].next = currEdge
+        }*/
+
+    }
+
+
+
+}
+
+
 //Print function for for_each loop
-void print(vect edg){
-    cout<<edg.first.first<<" "<<edg.first.second<<endl;
+void printGraph(Edge edg){
+    cout<<"{"<<edg.first.first<<" "<<edg.first.second<<"} w: "<<edg.second<<endl;
 }
